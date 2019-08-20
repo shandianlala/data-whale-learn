cb50f315edb8b596fee0df16dc09dcbe5dfcba75
shandianlala <chengxwang0622@163.com>
2019-08-20 23:02:56 +0800

    NumberIslands

---
+++ src/main/java/club/sdll/datawhale/learn/task6/NumberIslands.java
@@ -0,0 +1,38 @@
+package club.sdll.datawhale.learn.task6;
+
+/**
+ * @author shandianlala@gmail.com
+ * @description TODO
+ * Reference resources ：https://www.jianshu.com/p/b61010b9e836
+ * @since 2019-08-20 22:58
+ */
+public class NumberIslands {
+
+    private int n;
+    private int m;
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        n = grid.length;
+        if (n == 0) return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
+                }
+        }
+        return count;
+    }
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
+
+}
